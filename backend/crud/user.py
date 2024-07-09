aa0b0cdc445ef11f77976c4a6dc378369ea3ffc2
Ky_Dev25 <kylian.barthelemy2003@gmail.com>
2024-07-09 16:15:37 +0200

    black linter fixed

--- backend/crud/user.py
+++ backend/crud/user.py
@@ -8,8 +8,10 @@ from ..schemas.user import UserCreate
 def get_user(db: Session, user_id: int):
     return db.query(User).filter(User.id == user_id).first()
 
+
 def get_users(db: Session, skip: int = 0, limit: int = 10):
     return db.query(User).offset(skip).limit(limit).all()
+
 
 def create_user(db: Session, user: UserCreate):
     hashed_password = pwd_context.hash(user.password)
@@ -26,21 +28,23 @@ def create_user(db: Session, user: UserCreate):
     db.refresh(db_user)
     return db_user
 
+
 def update_user(db: Session, user_id: int, user_update: dict):
     db_user = db.query(User).filter(User.id == user_id).first()
     if db_user is None:
         raise ValueError("User not found")
 
     for key, value in user_update.items():
-        if key == 'password':
+        if key == "password":
             hashed_password = pwd_context.hash(value)
-            setattr(db_user, 'password_hashed', hashed_password)
+            setattr(db_user, "password_hashed", hashed_password)
         else:
             setattr(db_user, key, value)
 
     db.commit()
     db.refresh(db_user)
     return db_user
+
 
 def delete_user(db: Session, user_id: int):
     db_user = db.query(User).filter(User.id == user_id).first()
